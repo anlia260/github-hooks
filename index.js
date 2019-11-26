0c92311c76eaea5c355c580c176aa11afe9b2ab3
Fed.Akax <fed.akax@gmail.com>
2019-11-26 21:29:05 +0800

    init

---
+++ index.js
@@ -0,0 +1,45 @@
+var http = require("http");
+var createHandler = require("github-webhook-handler");
+var handler = createHandler({ path: "/github/webhook", secret: "Solo" });
+var exec = require("child_process").exec;
+
+http
+  .createServer(function(req, res) {
+    handler(req, res, function(err) {
+      res.statusCode = 404;
+      res.end("no such location");
+    });
+  })
+  .listen(7777);
+
+handler.on("error", function(err) {
+  console.error("Error:", err.message);
+});
+
+handler.on("push", function(event) {
+  console.log(
+    "Received a push event for %s to %s",
+    event.payload.repository.name,
+    event.payload.ref
+  );
+
+  var cmdStr = "sh ./deploy.sh";
+
+  exec(cmdStr, function(err, stdout, stderr) {
+    if (err) {
+      console.log("get weather api error:" + stderr);
+    } else {
+      console.log(stdout);
+    }
+  });
+});
+
+handler.on("issues", function(event) {
+  console.log(
+    "Received an issue event for %s action=%s: #%d %s",
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title
+  );
+});
